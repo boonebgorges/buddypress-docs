c0fb4101637ca213f0417d8544a780c25e486b06
Boone Gorges <boonebgorges@gmail.com>
2011-01-21 11:19:54 -0500

    Very basic hierarchy support

---
+++ includes/doc-hierarchy.php
@@ -0,0 +1,74 @@
+<?php
+
+/**
+ * This file contains the functions used to enable hierarchical docs.
+ * Separated into this file so that the feature can be turned off.
+ *
+ * @package BuddyPress Docs
+ */
+ 
+class BP_Docs_Hierarchy {	
+	/**
+	 * PHP 4 constructor
+	 *
+	 * @package BuddyPress Docs
+	 * @since 1.0
+	 */
+	function bp_docs_hierarchy() {
+		$this->__construct();
+	}
+
+	/**
+	 * PHP 5 constructor
+	 *
+	 * @package BuddyPress Docs
+	 * @since 1.0
+	 */	
+	function __construct() {
+		// Make sure that the bp_docs post type supports our post taxonomies
+		add_filter( 'bp_docs_post_type_args', array( $this, 'register_with_post_type' ) );
+	
+		// Hook into post saves to save any taxonomy terms. 
+		add_action( 'bp_docs_doc_saved', array( $this, 'save_post' ) );
+	}
+	
+	/**
+	 * Registers the post taxonomies with the bp_docs post type
+	 *
+	 * @package BuddyPress Docs
+	 * @since 1.0
+	 *
+	 * @param array The $bp_docs_post_type_args array created in BP_Docs::register_post_type()
+	 * @return array $args The modified parameters
+	 */	
+	function register_with_post_type( $args ) {
+		$args['hierarchical'] = true;
+		
+		return $args;		
+	}
+	
+	/**
+	 * Saves post parent to a doc when saved from the front end
+	 *
+	 * @package BuddyPress Docs
+	 * @since 1.0
+	 *
+	 * @param object $query The query object created by BP_Docs_Query
+	 * @return int $post_id Returns the doc's post_id on success
+	 */	
+	function save_post( $query ) {
+		if ( ! empty( $_POST['parent_id'] ) ) {
+			$args = array(
+				'ID' => $query->doc_id,
+				'post_parent' => (int) $_POST['parent_id']
+			);
+			
+			if ( $post_id = wp_update_post( $args ) )
+				return $post_id;
+			else
+				return false;
+		}
+	}
+}
+
+?>
