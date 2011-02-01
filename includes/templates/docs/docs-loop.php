63e80fe7c089e8049f778bdf5118d7af45901475
Boone Gorges <boonebgorges@gmail.com>
2011-01-31 19:00:47 -0500

    Begins to implement table structure for docs view

--- includes/templates/docs/docs-loop.php
+++ includes/templates/docs/docs-loop.php
@@ -1,15 +1,53 @@
 
 <?php include( apply_filters( 'bp_docs_header_template', $template_path . 'docs-header.php' ) ) ?>
 
+
 <?php if ( have_posts() ) : ?>
+	<table class="doctable">
 
-	<ul>
+	<thead>
+		<tr valign="bottom">
+			<th scope="column"> </th>
+			<th scope="column"><?php _e( 'Title', 'bpsp' ); ?></th>
+			<th scope="column"><?php _e( 'Author', 'bpsp' ); ?></th>
+			<th scope="column"><?php _e( 'Created', 'bpsp' ); ?></th>
+			<th scope="column"><?php _e( 'Last Edited', 'bpsp' ); ?></th>
+			<th scope="column"><?php _e( 'Tags', 'bpsp' ); ?></th>
+		</tr>
+        </thead>
+        
+        <tbody>
 	<?php while ( have_posts() ) : the_post() ?>
-		<li>
-			<h2><a href="<?php bp_docs_group_doc_permalink() ?>"><?php the_title() ?></a></h2>
-			<?php the_excerpt() ?>
-		</li>
-	<?php endwhile ?>
-	</ul>
+		<tr>
+			<td> </td>
+			
+			<td class="title-cell">
+				<a href="<?php bp_docs_group_doc_permalink() ?>"><?php the_title() ?></a>
+				
+				<?php the_excerpt() ?>
+			</td>
+			
+			<td class="author-cell">
+				<?php the_author() ?>
+			</td>
+			
+			<td class="date-cell created-date-cell"> 
+				<?php echo get_the_date() ?>
+			</td>
+			
+			<td class="date-cell edited-date-cell"> 
+				<?php echo get_the_modified_date() ?>
+			</td>
+			
+			<td class="tags-cell">
+				tags
+			</td>
+			
+			
+		</tr>
+	<?php endwhile ?>        
+        </tbody>
 
+
+	</table>
 <?php endif ?>
