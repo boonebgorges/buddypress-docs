cb07252702fedba6bf03c20850bc3d152cedfce2
Boone Gorges <boonebgorges@gmail.com>
2011-02-07 16:36:48 -0500

    Paving the way for filter markup

---
+++ includes/js/bp-docs.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function($){
+	/*(if ( $('.docs-info-header').length != false ) {
+		var dihp = $('.docs-info-header p');
+		var modlink = ' - <strong><a href="#" id="modify-filters">' + bp_docs.modifyfilters + '</a></strong>';
+		$(dihp).append(modlink);
+	}*/
+},(jQuery));
