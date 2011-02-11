484bf85b4ba1409976b3a4867a41a7208d07d394
Boone Gorges <boonebgorges@gmail.com>
2011-02-11 16:08:00 -0500

    Style changes on single doc screen

--- includes/templates/docs/single-doc.php
+++ includes/templates/docs/single-doc.php
@@ -4,14 +4,17 @@
 
 <h2><?php the_title() ?></h2> 
 
+<?php if ( bp_docs_current_user_can_edit() ) : ?>
+	<div class="doc-edit-link">
+		<a href="<?php bp_docs_group_doc_permalink() ?>/edit">Edit</a> 
+	</div>
+<?php endif ?>
+
 <?php the_content() ?>
 
 <div class="doc-meta">
 	<?php do_action( 'bp_docs_single_doc_meta' ) ?>
 </div>
 
-<?php if ( bp_docs_current_user_can_edit() ) : ?>
-	<a href="<?php bp_docs_group_doc_permalink() ?>/edit">Edit</a> 
-<?php endif ?>
 
 <?php endwhile; endif ?>
