be38c98581bee398f6e53e81657710b019967e31
Boone Gorges <boonebgorges@gmail.com>
2011-02-11 17:07:58 -0500

    Shuffles some js to account for slow loading and document.ready

--- includes/js/bp-docs.js
+++ includes/js/bp-docs.js
@@ -1,14 +1,4 @@
 jQuery(document).ready(function($){
-	/* Swap the 'filter by tag' text with a dummy link and hide tag list on load */
-	if ( $('p#filter-by-tag').length != false ) {
-		var c = $('p#filter-by-tag').html();
-		var cl = '<a href="#" class="doc-filter-toggle tag-filter-toggle">' + c + ' +</a>';
-		$('p#filter-by-tag').html(cl);
-		
-		/* Hide tags */
-		$('ul.filter-tags-list').toggle();
-	}
-	
 	/* When the filter toggle is clicked, show the tags. Todo: abstract for other filters */
 	$('.tag-filter-toggle').click(function(){
 		// Swap the +/- in the link
