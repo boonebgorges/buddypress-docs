051406823bbf3bd945baedf2cb81db2e73e1de38
boonebgorges <boonebgorges@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-26 09:39:46 +0000

    Adds some request catching infrastructure
    git-svn-id: http://plugins.svn.wordpress.org/buddypress-docs/trunk@364940 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/templates/docs/create-doc.php
@@ -0,0 +1,17 @@
+
+<?php include( apply_filters( 'bp_docs_header_template', $template_path . 'docs-header.php' ) ) ?>
+
+<form action="" method="post">
+
+	Title:
+	<input name="title" id="doc-title" value="" /> 
+	
+	<br />
+	
+	<textarea name="content" id="doc-content"></textarea>
+	
+	<input type="submit" value="<?php _e( 'Save', 'bp-docs' ) ?>" name="doc-edit-submit" />
+	
+	<a href="<?php bp_docs_group_doc_permalink() ?>">Cancel</a> 
+
+</form>
