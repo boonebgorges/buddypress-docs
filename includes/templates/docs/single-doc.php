1cc1e2afd587924d980be3fbee126f587cd0c896
boonebgorges <boonebgorges@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-26 09:35:20 +0000

    Adds subnav tabs for group docs section
    git-svn-id: http://plugins.svn.wordpress.org/buddypress-docs/trunk@364936 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/templates/docs/single-doc.php
+++ includes/templates/docs/single-doc.php
@@ -1,1 +1,9 @@
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post() ?>
+
+<?php include( apply_filters( 'bp_docs_header_template', $template_path . 'docs-header.php' ) ) ?>
+
+
 Single doc content markup will go here
+
+
+<?php endwhile; endif ?>
