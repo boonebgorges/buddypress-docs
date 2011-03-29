acd1b792b368fb2218ed44a79cfe52c015cd3179
boonebgorges <boonebgorges@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-29 20:45:53 +0000

    Adds formatting file. Shortens excerpts on docs page
    git-svn-id: http://plugins.svn.wordpress.org/buddypress-docs/trunk@366577 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/formatting.php
+++ includes/formatting.php
@@ -5,14 +5,14 @@
  * of BuddyPress Docs pages
  *
  * @package BuddyPress Docs
- * @since 1.0-beta
+ * @since 1.0
  */
 
 /**
  * Reduces the length of excerpts on the BP Docs doc list
  *
  * @package BuddyPress Docs
- * @since 1.0-beta
+ * @since 1.0
  *
  * @uses apply_filters() Plugins can filter bp_docs_excerpt_length to change the default
  * @param int $length WordPress's default excerpt length
@@ -26,24 +26,5 @@ function bp_docs_excerpt_length( $length ) {
 	return $length;
 }
 add_filter( 'excerpt_length', 'bp_docs_excerpt_length' );
-
-/**
- * Adds spaces after the commas in the tag edit textarea. Annoying that WP doesn't do this.
- *
- * @package BuddyPress Docs
- * @since 1.0-beta
- *
- * @param string $tags The string of tags
- * @return string $tags The string of tags with purdy commas
- */
-function bp_docs_tags_comma_space( $tags ) {
-	if ( bp_docs_is_bp_docs_page() ) {
-		$tags = explode( ',', $tags );
-		$tags = implode( ', ', $tags );
-	}
-	
-	return $tags;
-}
-add_filter( 'terms_to_edit', 'bp_docs_tags_comma_space' );
-
+ 
 ?>
