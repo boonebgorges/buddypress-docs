3fe3dd5b23fa86c42dd602d50b0f3ab7b34aba0e
boonebgorges <boonebgorges@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-29 20:32:09 +0000

    Adds subnav tabs for group docs section
    git-svn-id: http://plugins.svn.wordpress.org/buddypress-docs/trunk@366533 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/templatetags.php
+++ includes/templatetags.php
@@ -1,5 +1,23 @@
 <?php
 
+function bp_docs_group_tabs( $group = false ) {
+	global $bp, $groups_template, $post;
+	
+	if ( !$group )
+		$group = ( $groups_template->group ) ? $groups_template->group : $bp->groups->current_group;
 
+?>
+	<li<?php if ( $bp->bp_docs->current_view == 'list' ) : ?> class="current"<?php endif; ?>><a href="<?php echo $bp->root_domain . '/' . $bp->groups->slug ?>/<?php echo $group->slug ?>/<?php echo $bp->bp_docs->slug ?>/"><?php _e( 'View Docs', 'bp-docs' ) ?></a></li>
+
+	<?php /* Todo: can this user create items? */ ?>
+	<li<?php if ( 'create' == $bp->bp_docs->current_view ) : ?> class="current"<?php endif; ?>><a href="<?php echo $bp->root_domain . '/' . $bp->groups->slug ?>/<?php echo $group->slug ?>/<?php echo $bp->bp_docs->slug ?>/create"><?php _e( 'New Doc', 'bp-docs' ) ?></a></li>
+	
+	
+	<?php if ( $bp->bp_docs->current_view == 'single' || $bp->bp_docs->current_view == 'edit' ) : ?>
+		<li<?php if ( 'single' == $bp->bp_docs->current_view ) : ?> class="current"<?php endif; ?>><a href="<?php echo $bp->root_domain . '/' . $bp->groups->slug ?>/<?php echo $group->slug ?>/<?php echo $bp->bp_docs->slug ?>/<?php echo $post->post_name ?>"><?php the_title() ?></a></li>		
+	<?php endif ?>
+	
+<?php
+}
 
 ?>
