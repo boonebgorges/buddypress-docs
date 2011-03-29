5d20b4d6a7c00a4095f353eca8c47b9e28eb0d5d
boonebgorges <boonebgorges@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-29 20:29:52 +0000

    Adds first pass at basic loader class
    git-svn-id: http://plugins.svn.wordpress.org/buddypress-docs/trunk@366525 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- bp-docs.php
+++ bp-docs.php
@@ -1,145 +1,24 @@
 <?php
 
 class BP_Docs {
-	var $post_type_name;
-	var $associated_item_tax_name;
-	
 	/**
 	 * PHP 4 constructor
 	 *
 	 * @package BuddyPress Docs
-	 * @since 1.0-beta
+	 * @since 1.0
 	 */
 	function bp_docs() {
-		$this->__construct();
+		$this->construct();
 	}
 
 	/**
 	 * PHP 5 constructor
 	 *
 	 * @package BuddyPress Docs
-	 * @since 1.0-beta
+	 * @since 1.0
 	 */	
 	function __construct() {
-		global $bp;
-		
-		// Define post type and taxonomy names for use in the register functions
-		$this->post_type_name 		= apply_filters( 'bp_docs_post_type_name', 'bp_doc' );
-		$this->associated_item_tax_name = apply_filters( 'bp_docs_associated_item_tax_name', 'bp_docs_associated_item' );
-		
-		// Then stash them in the $bp global for use in template tags
-		$bp->bp_docs->post_type_name		= $this->post_type_name;
-		$bp->bp_docs->associated_item_tax_name	= $this->associated_item_tax_name;
-		
-		// Load predefined constants first thing
-		add_action( 'bp_docs_init', 	array( $this, 'load_constants' ), 2 );
-		
-		// Set up doc taxonomy
-		add_action( 'init', 		array( $this, 'load_doc_extras' ) );
-		
-		// Hooks into the 'init' action to register our WP custom post type and tax
-		add_action( 'init', 		array( $this, 'register_post_type' ) );
-		
-		// Includes necessary files
-		add_action( 'bp_docs_init', 	array( $this, 'includes' ), 4 );
-		
-		// Load the BP integration functions
-		add_action( 'bp_docs_init', 	array( $this, 'do_integration' ), 6 );
-
-		// Let plugins know that BP Docs has started loading
-		$this->init_hook();
-
-		// Let other plugins know that BP Docs has finished initializing
-		$this->loaded();
-	}
-	
-	/**
-	 * Defines bp_docs_init action
-	 *
-	 * This action fires on WP's init action and provides a way for the rest of BuddyPress
-	 * Docs, as well as other dependent plugins, to hook into the loading process in an
-	 * orderly fashion.
-	 *
-	 * @package BuddyPress Docs
-	 * @since 1.0-beta
-	 */
-	function init_hook() {
-		do_action( 'bp_docs_init' );
-	}
-	
-	/**
-	 * Defines bp_docs_loaded action
-	 *
-	 * This action tells BP Docs and other plugins that the main initialization process has
-	 * finished.
-	 *
-	 * @package BuddyPress Docs
-	 * @since 1.0-beta
-	 */
-	function loaded() {
-		do_action( 'bp_docs_loaded' );
-	}
-	
-	/**
-	 * Defines constants needed throughout the plugin.
-	 *
-	 * These constants can be overridden in bp-custom.php or wp-config.php.
-	 *
-	 * @package BuddyPress Docs
-	 * @since 1.0-beta
-	 */	
-	function load_constants() {
-		// You should never really need to override this bad boy
-		if ( !defined( 'BP_DOCS_INSTALL_PATH' ) )
-			define( 'BP_DOCS_INSTALL_PATH', dirname(__FILE__) . DIRECTORY_SEPARATOR );
-		
-		// Ditto
-		if ( !defined( 'BP_DOCS_INCLUDES_PATH' ) )
-			define( 'BP_DOCS_INCLUDES_PATH', BP_DOCS_INSTALL_PATH . 'includes' . DIRECTORY_SEPARATOR );
-		
-		// The main slug
-		if ( !defined( 'BP_DOCS_SLUG' ) )
-			define( 'BP_DOCS_SLUG', 'docs' );
-		
-		// The slug used when viewing a doc category
-		if ( !defined( 'BP_DOCS_CATEGORY_SLUG' ) )
-			define( 'BP_DOCS_CATEGORY_SLUG', 'category' );
-		
-		// The slug used when editing a doc
-		if ( !defined( 'BP_DOCS_EDIT_SLUG' ) )
-			define( 'BP_DOCS_EDIT_SLUG', 'edit' );
-		
-		// The slug used when viewing a single doc
-		if ( !defined( 'BP_DOCS_SINGLE_SLUG' ) )
-			define( 'BP_DOCS_SINGLE_SLUG', 'single' );
-			
-		// The slug used when creating a new doc
-		if ( !defined( 'BP_DOCS_CREATE_SLUG' ) )
-			define( 'BP_DOCS_CREATE_SLUG', 'create' );
-		
-		// The slug used when deleting a doc
-		if ( !defined( 'BP_DOCS_DELETE_SLUG' ) )
-			define( 'BP_DOCS_DELETE_SLUG', 'delete' );
-	}	
-	
-	/**
-	 * Loads the file that enables the use of extras (doc taxonomy, hierarchy, etc)
-	 *
-	 * This is loaded conditionally, so that the use of extras can be disabled by the
-	 * administrator. It is loaded before the bp_docs post type is registered so that we have
-	 * access to the 'taxonomy' argument of register_post_type.
-	 *
-	 * @package BuddyPress Docs
-	 * @since 1.0-beta
-	 */
-	function load_doc_extras() {
-		// Todo: make this conditional with a filter or a constant
-		require_once( BP_DOCS_INSTALL_PATH . DIRECTORY_SEPARATOR . 'includes' . DIRECTORY_SEPARATOR . 'addon-taxonomy.php' );
-		$this->taxonomy = new BP_Docs_Taxonomy;
-		
-		require_once( BP_DOCS_INSTALL_PATH . DIRECTORY_SEPARATOR . 'includes' . DIRECTORY_SEPARATOR . 'addon-hierarchy.php' );
-		$this->hierarchy = new BP_Docs_Hierarchy;
-		
+		add_action( 'init', array( $this, 'register_post_type' ) );	
 	}
 	
 	/**
@@ -156,9 +35,10 @@ class BP_Docs {
 	 * the taxonomy bp_docs_associated_item '6', where '6' is a sub-tax of 'groups'.
 	 *
 	 * @package BuddyPress Docs
-	 * @since 1.0-beta
+	 * @since 1.0
 	 */
 	function register_post_type() {
+
 		// Define the labels to be used by the post type bp_doc		
 		$post_type_labels = array(
 			'name' => _x( 'BuddyPress Docs', 'post type general name', 'bp-docs' ),
@@ -174,23 +54,19 @@ class BP_Docs {
 			'parent_item_colon' => ''
 		);
 	
-		// Set up the arguments to be used when the post type is registered
-		// Only filter this if you are hella smart and/or know what you're doing
-		$bp_docs_post_type_args = apply_filters( 'bp_docs_post_type_args', array(
+		// Register the bp_doc post type
+		register_post_type( 'bp_doc', array(
 			'label' => __( 'BuddyPress Docs', 'bp-docs' ),
 			'labels' => $post_type_labels,
-			'public' => false,
+			'public' => true,
 			'_builtin' => false,
 			'show_ui' => true,
 			'hierarchical' => false,
-			'supports' => array( 'title', 'editor', 'revisions', 'excerpt', 'comments' ),
+			'supports' => array( 'title', 'editor', 'revisions', 'excerpt' ),
 			'query_var' => true,
 			//'rewrite' => false
-			'rewrite' => false // Todo: This bites
-		) );
-	
-		// Register the bp_doc post type
-		register_post_type( $this->post_type_name, $bp_docs_post_type_args );
+			'rewrite' => array( "slug" => "doc", 'with_front' => false ), // Permalinks format
+		));
 		
 		// Define the labels to be used by the taxonomy bp_docs_associated_item
 		$associated_item_labels = array(
@@ -199,44 +75,13 @@ class BP_Docs {
 		);
 		
 		// Register the bp_docs_associated_item taxonomy
-		register_taxonomy( $this->associated_item_tax_name, array( $this->post_type_name ), array(
+		register_taxonomy( 'bp_docs_associated_item', array( 'bp_doc' ), array(
 			'labels' => $associated_item_labels,
 			'hierarchical' => true,
 			'show_ui' => true, 
 			'query_var' => true,
 			'rewrite' => array( 'slug' => 'item' ),
 		));
-	}
-	
-	/**
-	 * Includes files needed by BuddyPress Docs
-	 *
-	 * @package BuddyPress Docs
-	 * @since 1.0-beta
-	 */	
-	function includes() {
-	
-		// query-builder.php contains the class that fetches the content for each view
-		require_once( BP_DOCS_INCLUDES_PATH . 'query-builder.php' );
-		
-		// bp-integration.php provides the hooks necessary to hook into BP navigation
-		require_once( BP_DOCS_INCLUDES_PATH . 'integration-bp.php' );
-		
-		// templatetags.php has all functions in the global space available to templates
-		require_once( BP_DOCS_INCLUDES_PATH . 'templatetags.php' );
-		
-		// formatting.php contains filters and functions used to modify appearance only
-		require_once( BP_DOCS_INCLUDES_PATH . 'formatting.php' );
-	}	
-	
-	/**
-	 * Initiates the BP integration functions
-	 *
-	 * @package BuddyPress Docs
-	 * @since 1.0-beta
-	 */
-	function do_integration() {
-		$this->bp_integration = new BP_Docs_BP_Integration;	
 	}
 }
 
--- loader.php
+++ loader.php
@@ -1,26 +1,13 @@
 <?php
 /*
-Plugin Name: BuddyPress Wiki Component
-Plugin URI: http://wordpress.org/extend/plugins/bp-wiki/
-Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=A9NEGJEZR23H4
-Description: Enables site and group wiki functionality within a Buddypress install.
-Version: 1.0.0
-Revision Date: September 06, 2010
-Requires at least: WP 3.0.1, BuddyPress 1.2.5.2
-Tested up to: WP 3.0.2, BuddyPress 1.2.6
-License: AGPL http://www.fsf.org/licensing/licenses/agpl-3.0.html
-Author: David Cartwright
-Author URI: http://namoo.co.uk
+Plugin Name: BuddyPress Docs
 Network: true
 */
 
- 
-
 /* Only load the component if BuddyPress is loaded and initialized. */
-function bp_wiki_init() {
-
-	require( dirname( __FILE__ ) . '/includes/bp-wiki-core.php' );
-
+function bp_docs_init() {
+	require( dirname( __FILE__ ) . '/bp-docs.php' );
+	$bp_docs = new BP_Docs;
 }
-add_action( 'bp_init', 'bp_wiki_init' );
+add_action( 'bp_include', 'bp_docs_init' );
 ?>
