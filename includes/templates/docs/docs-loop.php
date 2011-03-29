6c08be9fbffdbbb9834b18bc6734ee88957575f7
boonebgorges <boonebgorges@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-29 21:06:00 +0000

    Adds pagination links and markers to docs list page. Fixes #49
    git-svn-id: http://plugins.svn.wordpress.org/buddypress-docs/trunk@366661 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/templates/docs/docs-loop.php
+++ includes/templates/docs/docs-loop.php
@@ -75,6 +75,16 @@
         
 	</table>
 	
+	<div id="bp-docs-pagination">
+		<div id="bp-docs-pagination-count">
+			<?php printf( __( 'Viewing %1$s-%2$s of %3$s docs', 'bp-docs' ), bp_docs_get_current_docs_start(), bp_docs_get_current_docs_end(), bp_docs_get_total_docs_num() ) ?>
+		</div>
+	
+		<div id="bp-docs-paginate-links">
+			<?php bp_docs_paginate_links() ?> 
+		</div>
+	</div>
+	
 <?php else: ?>
 
 	<p class="no-docs"><?php printf( __( 'There are no docs for this view. Why not <a href="%s">create one</a>?', 'bp-docs' ), bp_docs_get_item_docs_link() . BP_DOCS_CREATE_SLUG ) ?>
