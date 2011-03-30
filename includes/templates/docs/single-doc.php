614a25ea15137c6383b24b8d65ddb470e1ff6d1b
boonebgorges <boonebgorges@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-30 18:25:04 +0000

    Moves edit button next to title. Fixes #39
    git-svn-id: http://plugins.svn.wordpress.org/buddypress-docs/trunk@367126 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/templates/docs/single-doc.php
+++ includes/templates/docs/single-doc.php
@@ -2,19 +2,19 @@
 
 <?php include( apply_filters( 'bp_docs_header_template', $template_path . 'docs-header.php' ) ) ?>
 
-<h2><?php the_title() ?></h2> 
+<h2><?php the_title() ?> 
+	<?php if ( bp_docs_current_user_can( 'edit' ) ) : ?>
+		
+		<a class="button" href="<?php bp_docs_group_doc_permalink() ?>/edit"><?php _e( 'Edit', 'bp-docs' ) ?></a> 
+
+	<?php endif ?>
+</h2> 
 
 <?php the_content() ?>
 
 <div class="doc-meta">
 	<?php do_action( 'bp_docs_single_doc_meta' ) ?>
 </div>
-
-<?php if ( bp_docs_current_user_can( 'edit' ) ) : ?>
-	<div class="doc-edit-link">
-		<a href="<?php bp_docs_group_doc_permalink() ?>/edit"><?php _e( 'Edit this doc', 'bp-docs' ) ?></a> 
-	</div>
-<?php endif ?>
 
 <?php comments_template() ?>
 
