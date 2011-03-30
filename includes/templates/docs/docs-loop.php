478e2a792567da229dd32c93ea6e9917d068e6cb
boonebgorges <boonebgorges@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-30 18:06:20 +0000

    Adds links to individual docs plus supporting template tags
    git-svn-id: http://plugins.svn.wordpress.org/buddypress-docs/trunk@367049 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/templates/docs/docs-loop.php
+++ includes/templates/docs/docs-loop.php
@@ -6,7 +6,7 @@
 	<ul>
 	<?php while ( have_posts() ) : the_post() ?>
 		<li>
-			<h2><?php the_title() ?></h2>
+			<h2><a href="<?php bp_docs_group_doc_permalink() ?>"><?php the_title() ?></a></h2>
 			<?php the_content() ?>
 			<?php the_permalink() ?>
 		</li>
