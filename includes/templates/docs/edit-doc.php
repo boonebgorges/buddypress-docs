9dc37776c1162fd0030a346b3966ee94d62045ff
boonebgorges <boonebgorges@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-30 18:06:45 +0000

    Adds super simple markup for displaying and editing text
    git-svn-id: http://plugins.svn.wordpress.org/buddypress-docs/trunk@367051 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/templates/docs/edit-doc.php
+++ includes/templates/docs/edit-doc.php
@@ -1,1 +1,20 @@
-Edit functionality will go here.
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post() ?>
+
+<?php include( apply_filters( 'bp_docs_header_template', $template_path . 'docs-header.php' ) ) ?>
+
+<form action="" method="post">
+
+	Title:
+	<input name="title" id="doc-title" value="<?php the_title() ?>" /> 
+	
+	<br />
+	
+	<textarea name="content" id="doc-content"><?php echo get_the_content() ?></textarea>
+	
+	<input type="submit" value="<?php _e( 'Save Changes', 'bp-docs' ) ?>" name="doc-edit-submit" />
+	
+	<a href="<?php bp_docs_group_doc_permalink() ?>">Cancel</a> 
+
+</form>
+
+<?php endwhile; endif ?>
