9b233544e63d2ab729e8cba3f5cdaf46f0b45bc5
boonebgorges <boonebgorges@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-30 18:38:26 +0000

    Updates readme
    git-svn-id: http://plugins.svn.wordpress.org/buddypress-docs/trunk@367188 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ loader.php
@@ -0,0 +1,35 @@
+<?php
+/*
+Plugin Name: BuddyPress Docs
+Plugin URI: http://github.com/boonebgorges/buddypress-docs
+Description: Adds collaborative Docs to BuddyPress
+Version: 1.0-beta-2
+Author: Boone B Gorges
+Author URI: http://boonebgorges.com
+Licence: GPLv3
+Network: true
+*/
+
+/*
+This is beta software.
+Don't use it yet.
+
+Seriously!
+*/
+
+define( 'BP_DOCS_VERSION', '1.0-beta-2' );
+
+/**
+ * Loads BP Docs files only if BuddyPress is present
+ *
+ * @package BuddyPress Docs
+ * @since 1.0-beta
+ */
+function bp_docs_init() {
+	global $bp_docs;
+	
+	require( dirname( __FILE__ ) . '/bp-docs.php' );
+	$bp_docs = new BP_Docs;
+}
+add_action( 'bp_include', 'bp_docs_init' );
+?>
