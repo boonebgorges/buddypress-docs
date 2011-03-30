4d595857a82b4b57b1551f16076dad14af7b6116
boonebgorges <boonebgorges@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-30 18:17:54 +0000

    Adds slidetoggling for Filter By Tags
    git-svn-id: http://plugins.svn.wordpress.org/buddypress-docs/trunk@367098 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/js/bp-docs.js
+++ includes/js/bp-docs.js
@@ -1,7 +1,30 @@
 jQuery(document).ready(function($){
-	/*(if ( $('.docs-info-header').length != false ) {
-		var dihp = $('.docs-info-header p');
-		var modlink = ' - <strong><a href="#" id="modify-filters">' + bp_docs.modifyfilters + '</a></strong>';
-		$(dihp).append(modlink);
-	}*/
+	/* Swap the 'filter by tag' text with a dummy link and hide tag list on load */
+	if ( $('p#filter-by-tag').length != false ) {
+		var c = $('p#filter-by-tag').html();
+		var cl = '<a href="#" class="doc-filter-toggle tag-filter-toggle">' + c + ' +</a>';
+		$('p#filter-by-tag').html(cl);
+		
+		/* Hide tags */
+		$('ul.filter-tags-list').toggle();
+	}
+	
+	/* When the filter toggle is clicked, show the tags. Todo: abstract for other filters */
+	$('.tag-filter-toggle').click(function(){
+		// Swap the +/- in the link
+		var c = $(this).html();
+		var pom = c.substr( c.length - 1, 1 );
+		
+		if ( pom == '+' ) {
+			var mop = '-';
+		} else {
+			var mop = '+';
+		}
+		
+		$(this).html( c.substr( 0, c.length - 1 ) + mop );
+		
+		// Finally, slide the tags up or down
+		$('ul.filter-tags-list').slideToggle(400);
+	});
+	
 },(jQuery));
