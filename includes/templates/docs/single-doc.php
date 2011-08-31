33f7021a02f6ab45d5c2d776d38c2f775e2be8aa
Boone Gorges <boonebgorges@gmail.com>
2011-08-31 13:48:45 -0400

    Adds 'not found' message at the bottom of single-doc.php for incorrect URLs, with links to create new documents. Fixes #131. Props dwenaus

--- includes/templates/docs/single-doc.php
+++ includes/templates/docs/single-doc.php
@@ -7,7 +7,7 @@
 		<span class="toggle-switch" id="toggle-doc-is-locked"><?php _e( 'Locked', 'bp-docs' ) ?> <span class="hide-if-no-js description"><?php _e( '(click for more info)', 'bp-docs' ) ?></span></span>
 		<div class="toggle-content">
 			<p><?php printf( __( 'This doc is currently being edited by %1$s. In order to prevent edit conflicts, only one user can edit a doc at a time.', 'bp-docs' ), bp_docs_get_current_doc_locker_name() ) ?></p>
-			
+
 			<?php if ( is_super_admin() || bp_group_is_admin() ) : ?>
 				<p><?php printf( __( 'Please try again in a few minutes. Or, as an admin, you can <a href="%s">force cancel</a> the edit lock.', 'bp-docs' ), bp_docs_get_force_cancel_edit_lock_link() ) ?></p>
 			<?php else : ?>
@@ -15,7 +15,7 @@
 			<?php endif ?>
 		</div>
 	</div>
-		
+
 	<?php bp_docs_inline_toggle_js() ?>
 <?php endif ?>
 
@@ -29,4 +29,13 @@
 
 <?php comments_template( '/docs/comments.php' ) ?>
 
-<?php endwhile; endif ?>
+<?php endwhile; ?>
+
+<?php else : ?>
+
+	<p><?php _e( 'No Doc by this name exists.', 'bp-docs' ) ?></p>
+
+	<p><?php printf( __( '<a href="%1$s">View all Docs in this group</a> or <a href="%2$s">create a new Doc</a>.', 'bp-docs' ), bp_docs_get_item_docs_link(), bp_docs_get_item_docs_link() . 'create' ) ?></p>
+
+<?php endif; ?>
+
