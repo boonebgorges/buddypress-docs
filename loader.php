6d3e074d177fd2c71d9e5803f716658ca6ef8227
Boone Gorges <boonebgorges@gmail.com>
2012-02-02 09:25:15 -0500

    Bumps version number in branch to 1.2-bleeding

--- loader.php
+++ loader.php
@@ -3,7 +3,7 @@
 Plugin Name: BuddyPress Docs
 Plugin URI: http://github.com/boonebgorges/buddypress-docs
 Description: Adds collaborative Docs to BuddyPress
-Version: 1.1.19
+Version: 1.2-bleeding
 Author: Boone B Gorges
 Author URI: http://boone.gorg.es
 Licence: GPLv3
