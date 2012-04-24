16f7270102478f0aefe0b1be5aeba543f795c808
Boone Gorges <boonebgorges@gmail.com>
2012-04-24 14:08:16 -0400

    Removes stray header element from docs-header.php. Props hnla. Fixes #204

--- includes/templates/docs/docs-header.php
+++ includes/templates/docs/docs-header.php
@@ -6,27 +6,26 @@
 
 <?php if ( bp_docs_is_existing_doc() ) : ?>
 
-	<h2><?php the_title() ?></h2> 
+	<h2><?php the_title() ?></h2>
 
 	<div class="doc-tabs">
 		<ul>
-			<li<?php if ( 'single' == bp_docs_current_view() ) : ?> class="current"<?php endif ?>>	
-				<a href="<?php echo bp_docs_get_group_doc_permalink() ?>"><?php _e( 'Read', 'bp-docs' ) ?></a> 
+			<li<?php if ( 'single' == bp_docs_current_view() ) : ?> class="current"<?php endif ?>>
+				<a href="<?php echo bp_docs_get_group_doc_permalink() ?>"><?php _e( 'Read', 'bp-docs' ) ?></a>
 			</li>
-			
+
 			<?php if ( bp_docs_current_user_can( 'edit' ) ) : ?>
-				<li<?php if ( 'edit' == bp_docs_current_view() ) : ?> class="current"<?php endif ?>>	
-					<a href="<?php echo bp_docs_get_group_doc_permalink() . '/' . BP_DOCS_EDIT_SLUG ?>"><?php _e( 'Edit', 'bp-docs' ) ?></a> 
+				<li<?php if ( 'edit' == bp_docs_current_view() ) : ?> class="current"<?php endif ?>>
+					<a href="<?php echo bp_docs_get_group_doc_permalink() . '/' . BP_DOCS_EDIT_SLUG ?>"><?php _e( 'Edit', 'bp-docs' ) ?></a>
 				</li>
 			<?php endif ?>
-			
+
 			<?php do_action( 'bp_docs_header_tabs' ) ?>
 		</ul>
 	</div>
 
 <?php elseif ( 'create' == bp_docs_current_view() ) : ?>
-	
+
 	<h2><?php _e( 'New Doc', 'bp-docs' ); ?></h2>
 
 <?php endif ?>
-</h2> 
