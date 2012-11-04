7ba47a27355cba6276fa9d8a07f3f492d87a36f9
Boone B Gorges <boonebgorges@gmail.com>
2012-11-04 14:07:21 -0500

    More upgrades

--- includes/upgrade.php
+++ includes/upgrade.php
@@ -7,15 +7,9 @@
  */
 
 function bp_docs_upgrade_check() {
-	$upgrades        = array();
-	$current_version = BP_DOCS_VERSION;
-	$old_version     = bp_get_option( 'bp_docs_version' );
+	$upgrades = array();
 
-	if ( ! $old_version ) {
-		$old_version = '1.1';
-	}
-
-	if ( version_compare( $old_version, '1.2', '<' ) ) {
+	if ( ! bp_get_option( '_bp_docs_done_upgrade_1_2' ) ) {
 		$upgrades[] = '1.2';
 	}
 
@@ -27,17 +21,19 @@ function bp_docs_upgrade_init() {
 		return;
 	}
 
-	$upgrades = bp_docs_upgrade_check();
-
-	if ( ! empty( $upgrades ) ) {
-		add_action( 'admin_notices', 'bp_docs_upgrade_notice' );
-		bp_docs_upgrade_menu();
-	}
+	bp_docs_upgrade_menu();
+	add_action( 'admin_notices', 'bp_docs_upgrade_notice' );
 }
 add_action( 'admin_menu', 'bp_docs_upgrade_init' );
 
 function bp_docs_upgrade_notice() {
 	global $pagenow;
+
+	$upgrades = bp_docs_upgrade_check();
+
+	if ( ! empty( $_GET['bp_docs_upgraded'] ) ) {
+		echo '<div class="updated message"><p>' . __( 'Upgrade complete!', 'bp-docs' ) . '</p></div>';
+	}
 
 	if (
 		'edit.php' == $pagenow &&
@@ -102,9 +98,15 @@ function bp_docs_upgrade_render() {
 			<?php
 				$upgrade_status = bp_get_option( 'bp_docs_upgrade' );
 				$message        = isset( $upgrade_status['message'] ) ? $upgrade_status['message'] : '';
-				$refresh_url    = isset( $upgrade_status['refresh_url'] ) ? $upgrade_status['refresh_url'] : $url_base;
-				//$refresh_url = add_query_arg( 'do_upgrade', '1', $url_base );
-				//$refresh_url = wp_nonce_url( $refresh_url, 'bp-docs-upgrade' );
+
+				if ( isset( $upgrade_status['refresh_url'] ) ) {
+					$refresh_url = $upgrade_status['refresh_url'];
+				} else {
+					$refresh_url = add_query_arg( array(
+						'do_upgrade' => '1',
+						'_wpnonce'   => wp_create_nonce( 'bp-docs-upgrade' ),
+					), $url_base );
+				}
 			?>
 
 			<p><?php echo esc_html( $message ) ?></p>
@@ -116,7 +118,7 @@ function bp_docs_upgrade_render() {
 				function nextpage() {
 					location.href = "<?php echo $refresh_url ?>";
 				}
-				setTimeout( "nextpage()", 1000 );
+				setTimeout( "nextpage()", 2000 );
 				//-->
 			</script>
 
@@ -146,8 +148,9 @@ function bp_docs_do_upgrade() {
 			$func = 'bp_docs_upgrade_' . str_replace( '.', '_', $upgrade );
 			if ( function_exists( $func ) ) {
 				$upgrade_status['upgrades'][ $func ] = array(
-					'last' => '',
-					'total' => '',
+					'last' => 0,
+					'done' => 0,
+					'total' => 0,
 				);
 			}
 		}
@@ -161,11 +164,28 @@ function bp_docs_do_upgrade() {
 	}
 
 	if ( isset( $ufunc ) && isset( $udata ) ) {
-		$new_udata = call_user_func( $ufunc );
-		var_dump( $new_udata );
+		if ( intval( $udata['done'] ) <= intval( $udata['total'] ) ) {
+			$new_udata = call_user_func_array( $ufunc, array( $udata ) );
+			$upgrade_status['upgrades'][ $ufunc ] = $new_udata;
+
+			if ( isset( $new_udata['message'] ) ) {
+				$upgrade_status['message'] = $new_udata['message'];
+			}
+
+			if ( isset( $new_udata['refresh_url'] ) ) {
+				$upgrade_status['refresh_url'] = $new_udata['refresh_url'];
+			}
+		} else {
+			unset( $upgrade_status['upgrades'][ $ufunc ] );
+		}
+
 	} else {
-		// We're done
+		$upgrade_status['refresh_url'] = add_query_arg( array(
+			'bp_docs_upgraded' => 1,
+		), admin_url() );
 	}
+
+	bp_update_option( 'bp_docs_upgrade', $upgrade_status );
 }
 
 //////////////////////////////////////////////////
@@ -175,7 +195,95 @@ function bp_docs_do_upgrade() {
 //  - associated group tax terms change
 //
 //////////////////////////////////////////////////
-function bp_docs_upgrade_1_2() {
+function bp_docs_upgrade_1_2( $udata = array() ) {
+	global $wpdb;
 
+	$url_base = admin_url( add_query_arg( array(
+		'post_type' => bp_docs_get_post_type_name(),
+		'page' => 'bp-docs-upgrade',
+	), 'edit.php' ) );
+
+	if ( empty( $udata['total'] ) ) {
+		$udata['total'] = $wpdb->get_var( $wpdb->prepare( "SELECT COUNT(*) FROM $wpdb->posts WHERE post_type = %s", bp_docs_get_post_type_name() ) );
+	}
+
+	if ( ! isset( $udata['done'] ) ) {
+		$udata['done'] = 0;
+	}
+
+	if ( empty( $udata['group_terms_migrated'] ) ) {
+		$tn = bp_docs_get_associated_item_tax_name();
+
+		// Get the group parent term
+		$group_parent_term = term_exists( 'group', $tn );
+
+		// Get all the group terms
+		if ( $group_parent_term ) {
+
+			// Delete the cached children terms, for good measure
+			delete_option( $tn . '_children' );
+
+			$group_terms = get_terms( $tn, array(
+				'parent' => intval( $group_parent_term['term_id'] ),
+			) );
+
+			foreach ( $group_terms as $group_term ) {
+				// Concatenate new term slugs
+				$new_desc = sprintf( __( 'Docs associated with the group %s', 'bp-docs' ), $group_term->description );
+				$new_slug = 'bp_docs_associated_group_' . $group_term->name;
+				$new_name = $group_term->description;
+
+				wp_update_term( $group_term->term_id, $tn, array(
+					'description' => $new_desc,
+					'slug'        => $new_slug,
+					'name'        => $new_name,
+					'parent'      => 0,
+				) );
+			}
+
+			// Store that we're done
+			$udata['group_terms_migrated'] = 1;
+			$udata['message'] = __( 'Group terms migrated. Now migrating Doc access terms....', 'bp-docs' );
+			$udata['refresh_url'] = add_query_arg( array(
+				'do_upgrade' => '1',
+				'_wpnonce'   => wp_create_nonce( 'bp-docs-upgrade' ),
+			), $url_base );
+			$udata['total'] = 0;
+		}
+	} else if ( intval( $udata['done'] ) < intval( $udata['total'] ) ) {
+		$counter = 0;
+		while ( $counter < 5 ) {
+			$next_doc_id = $wpdb->get_var( $wpdb->prepare( "SELECT ID FROM $wpdb->posts WHERE post_type = %s AND ID > %d LIMIT 1", bp_docs_get_post_type_name(), intval( $udata['last'] ) ) );
+			if ( ! $next_doc_id ) {
+				$udata['done'] = $udata['total'];
+				$all_done = true;
+				break;
+			}
+
+			$doc_settings = get_post_meta( $next_doc_id, 'bp_docs_settings', true );
+			$read_setting = isset( $doc_settings['read'] ) ? $doc_settings['read'] : 'anyone';
+			bp_docs_update_doc_access( $next_doc_id, $read_setting );
+
+			$counter++;
+			$udata['done']++;
+			$udata['last'] = $next_doc_id;
+			$udata['message'] = sprintf( __( 'Migrated %s of %s Docs. Migrating....', 'bp-docs' ), $udata['done'], $udata['total'] );
+			$udata['refresh_url'] = add_query_arg( array(
+				'do_upgrade' => '1',
+				'_wpnonce'   => wp_create_nonce( 'bp-docs-upgrade' ),
+			), $url_base );
+		}
+	} else {
+		$all_done = true;
+		$udata['refresh_url'] = add_query_arg( array(
+			'bp_docs_upgraded' => 1,
+		), admin_url() );
+	}
+
+	if ( isset( $all_done ) ) {
+		bp_update_option( '_bp_docs_done_upgrade_1_2', 1 );
+	}
+
+	return $udata;
 }
 
