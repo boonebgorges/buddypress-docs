9fa6bbc7767885b5cb6c9dab98d220341f1d2fdc
Boone B Gorges <boonebgorges@gmail.com>
2012-11-18 16:16:53 -0500

    Proper l18n for 'Delete' string. FIxes #243

--- includes/templates/docs/single/edit.php
+++ includes/templates/docs/single/edit.php
@@ -137,7 +137,7 @@ if ( !function_exists( 'wp_editor' ) ) {
 
 		<input type="submit" name="doc-edit-submit" id="doc-edit-submit" value="<?php _e( 'Save', 'bp-docs' ) ?>"> <a href="<?php bp_docs_cancel_edit_link() ?>" class="action safe"><?php _e( 'Cancel', 'bp-docs' ); ?></a>
 
-            	<?php if ( bp_docs_current_user_can( 'manage' ) ) : ?><a class="delete-doc-button confirm" href="<?php bp_docs_delete_doc_link() ?>">Delete</a><?php endif ?>
+            	<?php if ( bp_docs_current_user_can( 'manage' ) ) : ?><a class="delete-doc-button confirm" href="<?php bp_docs_delete_doc_link() ?>"><?php _e( 'Delete', 'bp-docs' ) ?></a><?php endif ?>
         </div>
 
 
