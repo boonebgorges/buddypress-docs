532b0ac728b8c871d14f042b1e7dc2f2cb920533
Boone B Gorges <boonebgorges@gmail.com>
2013-02-26 01:23:47 -0500

    Make sure that tests load BP

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,10 @@
 <?php
 
 $GLOBALS['wp_tests_options'] = array(
-    'active_plugins' => array( basename( dirname( dirname( __FILE__ ) ) ) . '/buddypress-docs.php' ),
+    'active_plugins' => array(
+	basename( dirname( dirname( __FILE__ ) ) ) . '/loader.php',
+	'buddypress/bp-loader.php',
+    ),
 );
 
 require getenv( 'WP_TESTS_DIR' ) . '/includes/bootstrap.php';
---
+++ tests/test-bp-docs.php
@@ -0,0 +1,10 @@
+<?php
+
+class BP_Docs_Tests extends WP_UnitTestCase {
+
+	function testSample() {
+		// replace this with some actual testing code
+		$this->assertTrue( function_exists( 'bp_core_get_users' ) );
+	}
+}
+
