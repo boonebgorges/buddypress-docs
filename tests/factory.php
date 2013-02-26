64ffc633f0bcc45a915b7e49057ad51af0a74d00
Boone B Gorges <boonebgorges@gmail.com>
2013-02-26 01:55:51 -0500

    Adds BP bootstrap and installer

---
+++ tests/factory.php
@@ -0,0 +1,35 @@
+<?php
+
+class BP_Docs_UnitTest_Factory_For_Doc extends WP_UnitTest_Factory_For_Thing {
+
+	function __construct( $factory = null ) {
+		parent::__construct( $factory );
+		$this->default_generation_definitions = array(
+			'post_status' => 'publish',
+			'post_title' => new WP_UnitTest_Generator_Sequence( 'Doc title %s' ),
+			'post_content' => new WP_UnitTest_Generator_Sequence( 'Doc content %s' ),
+			'post_type' => 'bp_doc'
+		);
+	}
+
+	function create_object( $args ) {
+		$post_id = wp_insert_post( $args );
+
+		/*
+		if ( isset( $args['task_list_id'] ) ) {
+			update_post_meta( $post_id, '_cp-task-list-id', $args['task_list_id'] );
+		}
+		*/
+
+		return $post_id;
+	}
+
+	/**
+	 * @todo
+	 */
+	function update_object( $object, $fields ) {}
+
+	function get_object_by_id( $post_id ) {
+		return get_post( $post_id );
+	}
+}
--- tests/test-bp-docs.php
+++ tests/test-bp-docs.php
@@ -2,9 +2,28 @@
 
 class BP_Docs_Tests extends WP_UnitTestCase {
 
-	function testSample() {
-		// replace this with some actual testing code
-		$this->assertTrue( function_exists( 'bp_core_get_users' ) );
+	function setUp() {
+		parent::setUp();
+
+		// @todo Temporary implementation. For now I'm shipping the BP
+		// factory with the plugin
+
+
+		require_once( dirname(__FILE__) . '/bp-factory.php' );
+		$this->factory->activity = new BP_UnitTest_Factory_For_Activity( $this->factory );
+		$this->factory->group = new BP_UnitTest_Factory_For_Group( $this->factory );
+
+		require_once( dirname(__FILE__) . '/factory.php' );
+		$this->factory->doc = new BP_Docs_UnitTest_Factory_For_Doc( $this->factory );
+	}
+
+	function test_delete_activity_on_doc_deletion() {
+		$doc_id = $this->factory->doc->create();
+
+		global $bp, $wpdb;
+		$this->assertTrue( function_exists( 'bp_activity_get' ) );
+
 	}
 }
+
 
