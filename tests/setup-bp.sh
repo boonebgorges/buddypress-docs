9a6782e814e82daeb38fa1ade4f3d41296668f1f
Boone B Gorges <boonebgorges@gmail.com>
2013-05-24 12:14:59 -0400

    Go to the plugin dir at the end of setup-bp.sh, to prepare for 'phpunit'

--- tests/setup-bp.sh
+++ tests/setup-bp.sh
@@ -2,13 +2,14 @@
 
 set -x
 
-export WP_CORE_DIR=/tmp/wordpress
-
 buddypress_dir=$WP_CORE_DIR/wp-content/plugins/buddypress
 
 # Grab specified version of BP
 wget -nv -O /tmp/buddypress.tar.gz https://github.com/BuddyPress/BuddyPress/tarball/$BP_VERSION
 mkdir -p $buddypress_dir
 tar --strip-components=1 -zxmf /tmp/buddypress.tar.gz -C $buddypress_dir
+
+# go back to the plugin dir
+cd $plugin_dir
 
 set +x
