203d4efaa493bf7c6ca6a21f4b691c96e715cc30
Boone B Gorges <boonebgorges@gmail.com>
2013-08-22 15:05:09 -0400

    Modify activity args when posting a group-associated doc
    This is necessary to ensure that the edits show up in the group activity
    stream.
    
    Note that this method will not work correctly when associating with
    multiple docs. More research is needed.
    
    Note also that this change means that we can't depend on the doc id
    being the doc. This is bound to cause problems when using the activity
    stream in advanced ways, but there is no good alternative at this point.
    
    Based on an original implementation by @r-a-y in r-a-y/buddypress-docs@88841ea1f12399ea79184bf5398d59ca29329584
    
    See #353

--- includes/integration-groups.php
+++ includes/integration-groups.php
@@ -53,6 +53,7 @@ class BP_Docs_Groups_Integration {
 
 		// Filter the activity actions for group docs-related activity
 		add_filter( 'bp_docs_activity_action',		array( $this, 'activity_action' ), 10, 5 );
+		add_filter( 'bp_docs_activity_args',            array( $this, 'activity_args' ), 10, 2 );
 		add_filter( 'bp_docs_comment_activity_action',	array( $this, 'comment_activity_action' ), 10, 5 );
 
 		// Filter the activity hide_sitewide parameter to respect group privacy levels
@@ -515,6 +516,29 @@ class BP_Docs_Groups_Integration {
 		return $action;
 	}
 
+	/**
+	 * Modify activity arguments before saving so newly-created group docs are
+	 * added into the group activity stream.
+	 *
+	 * @since 1.4.6
+	 *
+	 * @param array $args Activity arguments
+	 * @param obj $query The BP Docs query object
+	 * @return array
+	 */
+	public function activity_args( $args, $query ) {
+		global $bp;
+
+		$doc_id = isset( $query->doc_id ) ? (int) $query->doc_id : 0;
+		$group_id = bp_docs_get_associated_group_id( $doc_id );
+
+		if ( ! empty( $group_id ) ) {
+			$args['component'] = $bp->groups->id;
+			$args['item_id'] = $group_id;
+		}
+
+		return $args;
+	}
 
 	/**
 	 * Filters the activity action of 'new doc comment' activity to include the group name
