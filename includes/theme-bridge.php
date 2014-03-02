163b78cf65d006fbb5883139d76282fd5b70d242
Boone B Gorges <boonebgorges@gmail.com>
2014-03-02 14:26:25 -0500

    Don't let BP toggle off is_page during theme compatibility
    We need it to remain on, because we use comments_template()

--- includes/theme-bridge.php
+++ includes/theme-bridge.php
@@ -131,6 +131,9 @@ class BP_Docs_Theme_Compat {
 			} else {
 				$this->single_content_template = 'docs/single/index';
 				add_filter( 'bp_docs_allow_comment_section', '__return_false' );
+
+				// Necessary as of BP 1.9.2
+				remove_action( 'bp_replace_the_content', 'bp_theme_compat_toggle_is_page', 9999 );
 			}
 
 			add_action( 'bp_template_include_reset_dummy_post_data', array( $this, 'single_dummy_post' ) );
