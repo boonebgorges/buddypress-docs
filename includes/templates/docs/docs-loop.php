49d8673491deb0c98f749c3346e4998f3fb5cff1
Boone B Gorges <boonebgorges@gmail.com>
2014-06-24 11:41:45 -0400

    Correct template switching logic in docs-loop.php

--- includes/templates/docs/docs-loop.php
+++ includes/templates/docs/docs-loop.php
@@ -10,6 +10,8 @@
 	<?php bp_locate_template( 'docs/docs-loop-tree.php', true ) ?>
 <?php elseif ( bp_docs_is_folder_manage_view() ) : ?>
 	<?php bp_locate_template( 'docs/manage-folders.php', true ) ?>
+<?php else : ?>
+	<?php bp_locate_template( 'docs/docs-loop-list.php', true ) ?>
 <?php endif; ?>
 
 </div><!-- /#buddypress -->
