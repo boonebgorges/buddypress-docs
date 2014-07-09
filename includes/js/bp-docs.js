boonebgorges/buddypress-docs

commit 0a15b0af987ee220b0f2d31343e2d7190f8ccd9c

Don't unset idle lock when clicking metabox toggle <a> elements

includes/js/bp-docs.js
@@ -245,7 +245,11 @@ function bp_docs_load_idle() {
 		}
 
 		/* Remove the edit lock when the user clicks away */
-		jQuery("a").click(function(){
+		jQuery( "a" ).click(function( event ){
+			if ( $( event.target ).hasClass( 'toggle-link' ) ) {
+				return true;
+			}
+
 			var doc_id = jQuery("#existing-doc-id").val();
 			var data = {action:'remove_edit_lock', doc_id:doc_id};
 			jQuery.ajax({

commit 7205200462b5f03ed04f15d010c9ecaae39942c7

Fix $/jQuery reference error introduced in 0a15b0af

includes/js/bp-docs.js
@@ -246,7 +246,7 @@ function bp_docs_load_idle() {
 
 		/* Remove the edit lock when the user clicks away */
 		jQuery( "a" ).click(function( event ){
-			if ( $( event.target ).hasClass( 'toggle-link' ) ) {
+			if ( jQuery( event.target ).hasClass( 'toggle-link' ) ) {
 				return true;
 			}
 