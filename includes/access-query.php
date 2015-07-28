boonebgorges/buddypress-docs

commit 99b75666b5b7863fe0544016e4c56a11d6062764

Use WP_Query directly in doc access protection.
Using `WP_Query` allows the use of the `fields` argument, so we can
request just the needed post IDs rather than returning the entire post
object and plucking the ID.

Fixes #475.

includes/access-query.php
@@ -113,7 +113,7 @@ class BP_Docs_Access_Query {
 		if ( empty( $tax_query ) ) {
 			$forbidden_fruit_ids = array();
 		} else {
-			$forbidden_fruit = get_posts( array(
+			$forbidden_fruit_ids = new WP_Query( array(
 				'post_type' => bp_docs_get_post_type_name(),
 				'posts_per_page' => -1,
 				'nopaging' => true,
@@ -121,9 +121,8 @@ class BP_Docs_Access_Query {
 				'update_post_term_cache' => false,
 				'update_post_meta_cache' => false,
 				'no_found_rows' => 1,
+				'fields' => 'ids',
 			) );
-
-			$forbidden_fruit_ids = wp_list_pluck( $forbidden_fruit, 'ID' );
 		}
 
 		add_action( 'pre_get_posts', 'bp_docs_general_access_protection', 28 );

commit 9f6d067b2140d01ecbe042a0a07ff8fdc795f689

BP_Docs_Access_Query::get_doc_ids() should return an array.
Correct a problem introduced in 99b7566, in which the query argument
was returned when only the `posts` array should have been returned.

includes/access-query.php
@@ -113,7 +113,7 @@ class BP_Docs_Access_Query {
 		if ( empty( $tax_query ) ) {
 			$forbidden_fruit_ids = array();
 		} else {
-			$forbidden_fruit_ids = new WP_Query( array(
+			$forbidden_fruit = new WP_Query( array(
 				'post_type' => bp_docs_get_post_type_name(),
 				'posts_per_page' => -1,
 				'nopaging' => true,
@@ -123,6 +123,7 @@ class BP_Docs_Access_Query {
 				'no_found_rows' => 1,
 				'fields' => 'ids',
 			) );
+			$forbidden_fruit_ids = $forbidden_fruit->posts;
 		}
 
 		add_action( 'pre_get_posts', 'bp_docs_general_access_protection', 28 );