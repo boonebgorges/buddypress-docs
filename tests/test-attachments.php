b1040f72cacd6e350addc10bb1d74a1ec2bca9c4
Boone B Gorges <boonebgorges@gmail.com>
2016-04-24 10:35:28 -0500

    Add `@group` annotation to attachment tests.

--- tests/test-attachments.php
+++ tests/test-attachments.php
@@ -1,5 +1,8 @@
 <?php
 
+/**
+ * @group attachments
+ */
 class BP_Docs_Attachments_Tests extends BP_Docs_TestCase {
 	function test_filename_is_safe() {
 		$this->assertTrue( BP_Docs_Attachments::filename_is_safe( 'foo.jpg' ) );
