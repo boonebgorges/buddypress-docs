369384f75e8267549b67b3e4baa4647cf97a1a90
Boone B Gorges <boonebgorges@gmail.com>
2016-04-24 10:35:02 -0500

    Don't allow an activity item to be created when an unchanged revision is saved.

--- tests/test-activity.php
+++ tests/test-activity.php
@@ -4,6 +4,8 @@
  * @group activity
  */
 class BP_Docs_Tests_Activity extends BP_Docs_TestCase {
+	protected $current_doc;
+
 	/**
 	 * @ticket 536
 	 */
@@ -63,5 +65,121 @@ class BP_Docs_Tests_Activity extends BP_Docs_TestCase {
 		$post = $old_post;
 
 		$this->assertSame( 'foo', $found );
+	}
+
+	public function test_edit_activity_should_be_created_for_changed_content() {
+		// We have to do unholy things to make this testable.
+		$old_post = $_POST;
+
+		$doc = $this->factory->doc->create( array(
+			'post_content' => 'foo',
+			'post_title' => 'Test Doc',
+			'post_name' => 'test-doc',
+		) );
+
+		$this->current_doc = get_post( $doc );
+		add_filter( 'bp_docs_get_current_doc', array( $this, 'filter_current_doc' ) );
+
+		$_POST = array(
+			'doc' => array( 'title' => 'Test Doc' ),
+			'doc_content' => 'foo bar',
+			'ID' => $doc,
+		);
+
+		$q = new BP_Docs_Query();
+		$q->doc_slug = 'test-doc';
+		$q->save();
+
+		remove_filter( 'bp_docs_get_current_doc', array( $this, 'filter_current_doc' ) );
+		$_POST = $old_post;
+
+		$found = bp_activity_get( array(
+			'show_hidden'	=> 1,
+			'filter'	=> array(
+				'action' => 'bp_doc_edited',
+				'secondary_id' => $doc,
+			),
+		) );
+
+		$this->assertNotEmpty( $found['activities'] );
+	}
+
+	public function test_edit_activity_should_be_created_for_changed_title() {
+		// We have to do unholy things to make this testable.
+		$old_post = $_POST;
+
+		$doc = $this->factory->doc->create( array(
+			'post_content' => 'foo',
+			'post_title' => 'Test Doc',
+			'post_name' => 'test-doc',
+		) );
+
+		$this->current_doc = get_post( $doc );
+		add_filter( 'bp_docs_get_current_doc', array( $this, 'filter_current_doc' ) );
+
+		$_POST = array(
+			'doc' => array( 'title' => 'Test Doc Foo' ),
+			'doc_content' => 'foo',
+			'ID' => $doc,
+		);
+
+		$q = new BP_Docs_Query();
+		$q->doc_slug = 'test-doc';
+		$q->save();
+
+		remove_filter( 'bp_docs_get_current_doc', array( $this, 'filter_current_doc' ) );
+		$_POST = $old_post;
+
+		$found = bp_activity_get( array(
+			'show_hidden'	=> 1,
+			'filter'	=> array(
+				'action' => 'bp_doc_edited',
+				'secondary_id' => $doc,
+			),
+		) );
+
+		$this->assertNotEmpty( $found['activities'] );
+	}
+
+	public function test_edit_activity_should_not_be_created_for_unchanged_revision() {
+		// We have to do unholy things to make this testable.
+		$old_post = $_POST;
+
+		$doc = $this->factory->doc->create( array(
+			'post_content' => 'foo',
+			'post_title' => 'Test Doc',
+			'post_name' => 'test-doc',
+		) );
+
+		$this->current_doc = get_post( $doc );
+		add_filter( 'bp_docs_get_current_doc', array( $this, 'filter_current_doc' ) );
+
+		$_POST = array(
+			'doc' => array( 'title' => 'Test Doc' ),
+			'doc_content' => 'foo',
+			'ID' => $doc,
+		);
+
+		$q = new BP_Docs_Query();
+		$q->doc_slug = 'test-doc';
+		$q->save();
+
+		remove_filter( 'bp_docs_get_current_doc', array( $this, 'filter_current_doc' ) );
+		$_POST = $old_post;
+
+		$found = bp_activity_get( array(
+			'show_hidden'	=> 1,
+			'filter'	=> array(
+				'action' => 'bp_doc_edited',
+				'secondary_id' => $doc,
+			),
+		) );
+
+		$this->assertSame( array(), $found['activities'] );
+	}
+
+	public function filter_current_doc( $doc_id ) {
+//		return $doc_id;
+		return $this->current_doc;
 	}
 }
