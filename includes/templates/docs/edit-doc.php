54ec1ca2f8a719d30c0209ad57bee30fc27e0f96
Frank Staude <frank@staude.net>
2017-09-07 18:18:08 +0200

    replace basename() with wp_basename()
    replaced basename() with wp_basename(() calls for multibyte character path handling.
    see https://core.trac.wordpress.org/ticket/21217

--- includes/templates/docs/edit-doc.php
+++ includes/templates/docs/edit-doc.php
@@ -5,6 +5,6 @@
  * @package BuddyPress_Docs
  */
 
-_deprecated_file( basename(__FILE__), '1.2', BP_DOCS_INCLUDES_PATH_ABS . 'templates/docs/single/edit.php' );
+_deprecated_file( wp_basename(__FILE__), '1.2', BP_DOCS_INCLUDES_PATH_ABS . 'templates/docs/single/edit.php' );
 require_once ( BP_DOCS_INCLUDES_PATH . 'templates/docs/single/edit.php');
 ?>
