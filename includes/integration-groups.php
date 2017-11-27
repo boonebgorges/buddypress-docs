0440702a97bd93370b4f3ebb75de98b86b3bcce8
Boone B Gorges <boonebgorges@gmail.com>
2017-11-27 16:25:45 +0000

    Use `home_url()` to calculate 'bp-docs-last-docs-directory' cookie value.
    This ensures that it works properly on subdirectory setups.
    
    Props @ReneLuecking.
    Fixes #574.

--- includes/integration-groups.php
+++ includes/integration-groups.php
@@ -1008,8 +1008,10 @@ class BP_Docs_Groups_Integration {
 	 * @since 1.9.0
 	 */
 	public function	set_directory_cookie() {
+		global $wp;
+
 		if ( bp_docs_is_group_docs() ) {
-			@setcookie( 'bp-docs-last-docs-directory', home_url( $_SERVER['REQUEST_URI'] ), 0, '/' );
+			@setcookie( 'bp-docs-last-docs-directory', home_url( $wp->request ), 0, '/' );
 		}
 	}
 }
