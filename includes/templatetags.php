6530b72b47414387567689e2aae2083b68ea5bae
Boone B Gorges <boonebgorges@gmail.com>
2018-04-23 21:30:16 -0500

    Directory tab support for Nouveau.
    This commit moves tab markup into template files. Separate template files
    are included for Nouveau and Legacy.
    
    See #622.

--- includes/templatetags.php
+++ includes/templatetags.php
@@ -1550,32 +1550,21 @@ function bp_docs_get_docs_slug() {
  * @todo Get the group stuff out
  */
 function bp_docs_tabs( $show_create_button = true ) {
+	$theme_package = bp_get_theme_package_id();
 
-	?>
+	switch ( bp_get_theme_package_id() ) {
+		case 'nouveau' :
+			$template = 'tabs-nouveau.php';
+		break;
 
-	<ul id="bp-docs-all-docs">
-		<li<?php if ( bp_docs_is_global_directory() ) : ?> class="current"<?php endif; ?>><a href="<?php bp_docs_archive_link() ?>"><?php _e( 'All Docs', 'buddypress-docs' ) ?></a></li>
+		default :
+			$template = 'tabs-legacy.php';
+		break;
+	}
 
-		<?php if ( is_user_logged_in() ) : ?>
-			<?php if ( function_exists( 'bp_is_group' ) && bp_is_group() ) : ?>
-				<li<?php if ( bp_is_current_action( BP_DOCS_SLUG ) ) : ?> class="current"<?php endif ?>><a href="<?php bp_group_permalink( groups_get_current_group() ) ?><?php bp_docs_slug() ?>"><?php printf( __( "%s's Docs", 'buddypress-docs' ), bp_get_current_group_name() ) ?></a></li>
-			<?php else : ?>
-				<li><a href="<?php bp_docs_mydocs_started_link() ?>"><?php _e( 'Started By Me', 'buddypress-docs' ) ?></a></li>
-				<li><a href="<?php bp_docs_mydocs_edited_link() ?>"><?php _e( 'Edited By Me', 'buddypress-docs' ) ?></a></li>
-
-				<?php if ( bp_is_active( 'groups' ) ) : ?>
-					<li<?php if ( bp_docs_is_mygroups_docs() ) : ?> class="current"<?php endif; ?>><a href="<?php bp_docs_mygroups_link() ?>"><?php _e( 'My Groups', 'buddypress-docs' ) ?></a></li>
-				<?php endif ?>
-			<?php endif ?>
-
-		<?php endif ?>
-
-		<?php if ( $show_create_button ) : ?>
-			<?php bp_docs_create_button() ?>
-		<?php endif ?>
-
-	</ul>
-	<?php
+	// Calling `include` here so `$show_create_button` is in template scope.
+	$located = bp_docs_locate_template( $template );
+	include( $located );
 }
 
 /**
