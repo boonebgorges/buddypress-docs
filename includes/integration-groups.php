56672d9a07d91f97725018a629e3eb06de02b292
Boone B Gorges <boonebgorges@gmail.com>
2018-05-17 21:26:16 -0500

    Simplify markup on group > Manage panel.
    Fixes #416.

--- includes/integration-groups.php
+++ includes/integration-groups.php
@@ -1251,23 +1251,16 @@ class BP_Docs_Group_Extension extends BP_Group_Extension {
 		<div id="group-doc-options" <?php if ( !$group_enable ) : ?>class="hidden"<?php endif ?>>
 			<h3><?php _e( 'Options', 'buddypress-docs' ) ?></h3>
 
-			<table class="group-docs-options">
-				<tr>
-					<td class="label">
-						<label for="bp-docs-can-create"><?php _e( 'Minimum role to associate Docs with this group:', 'buddypress-docs' ) ?></label>
-					</td>
-
-					<td>
-						<select name="bp-docs[can-create]" id="bp-docs-can-create">
-							<option value="admin" <?php selected( $can_create, 'admin' ) ?>><?php _e( 'Group admin', 'buddypress-docs' ) ?></option>
-							<option value="mod" <?php selected( $can_create, 'mod' ) ?>><?php _e( 'Group moderator', 'buddypress-docs' ) ?></option>
-							<option value="member" <?php selected( $can_create, 'member' ) ?>><?php _e( 'Group member', 'buddypress-docs' ) ?></option>
-						</select>
-					</td>
-				</tr>
-
-			</table>
+			<label for="bp-docs-can-create"><?php _e( 'Minimum role to associate Docs with this group:', 'buddypress-docs' ) ?></label>
+			<select name="bp-docs[can-create]" id="bp-docs-can-create">
+				<option value="admin" <?php selected( $can_create, 'admin' ) ?>><?php _e( 'Group admin', 'buddypress-docs' ) ?></option>
+				<option value="mod" <?php selected( $can_create, 'mod' ) ?>><?php _e( 'Group moderator', 'buddypress-docs' ) ?></option>
+				<option value="member" <?php selected( $can_create, 'member' ) ?>><?php _e( 'Group member', 'buddypress-docs' ) ?></option>
+			</select>
 		</div>
+
+		<?php /* History's laziest way to create spacing without loading stylesheet */ ?>
+		<p></p>
 
 		<?php
 	}
