531cd211d7dc2c47aa45dd4bcdacad28594f1c2b
Boone B Gorges <boonebgorges@gmail.com>
2023-10-25 13:11:06 -0500

    Use `bp_members_get_user_url()` instead of `bp_core_get_user_domain()` in docs loop template.
    See #727.

--- includes/templates/docs/docs-loop.php
+++ includes/templates/docs/docs-loop.php
@@ -142,7 +142,7 @@ if ( ! $bp_docs_do_theme_compat ) : ?>
 
 				<?php if ( ! bp_docs_is_started_by() ) : ?>
 					<td class="author-cell">
-						<a href="<?php echo bp_core_get_user_domain( get_the_author_meta( 'ID' ) ) ?>" title="<?php echo bp_core_get_user_displayname( get_the_author_meta( 'ID' ) ) ?>"><?php echo bp_core_get_user_displayname( get_the_author_meta( 'ID' ) ) ?></a>
+						<a href="<?php echo esc_url( bp_members_get_user_url( get_the_author_meta( 'ID' ) ) ); ?>" title="<?php echo bp_core_get_user_displayname( get_the_author_meta( 'ID' ) ) ?>"><?php echo bp_core_get_user_displayname( get_the_author_meta( 'ID' ) ) ?></a>
 					</td>
 				<?php endif; ?>
 
